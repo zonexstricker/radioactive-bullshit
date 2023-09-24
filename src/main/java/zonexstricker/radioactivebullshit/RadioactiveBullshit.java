e9429e84e9138bbdfbeebddbb8d6b67942655b88
olivia <oscarcraftstyle@gmail.com>
2023-09-24 22:49:49 +0100

    Added raw uranium fully Fixed mod id conflict

--- src/main/java/zonexstricker/radioactivebullshit/RadioactiveBullshit.java
+++ src/main/java/zonexstricker/radioactivebullshit/RadioactiveBullshit.java
@@ -8,10 +8,18 @@ import turniplabs.halplibe.helper.ItemHelper;
 
 
 public class RadioactiveBullshit implements ModInitializer {
+
+    static {
+        //this is here to possibly fix some class loading issues, do not delete
+        try {
+            Class.forName("net.minecraft.core.block.Block");
+            Class.forName("net.minecraft.core.item.Item");
+        } catch (ClassNotFoundException ignored) {}
+    }
     public static final String MOD_ID = "radioactivebullshit";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final Item uraniumRaw = ItemHelper.createItem(MOD_ID, new Item("uranium.raw", 32750),"uraniumRaw", "uranium_raw.png");
+    public static final Item uraniumRaw = ItemHelper.createItem(MOD_ID, new Item(32750),"uraniumRaw", "uranium_raw.png");
     @Override
     public void onInitialize() {
         LOGGER.info("ExampleMod initialized.");
